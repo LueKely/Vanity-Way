98ab1dd1588347c32def0e9b775f92f5a9c57057
LueKely <99306129+LueKely@users.noreply.github.com>
2022-05-31 23:10:08 +0800

    Final I hope please work

--- testing.js
+++ testing.js
@@ -2,12 +2,45 @@ const faqP = document.querySelectorAll('.faq-p');
 const faqH1 = document.querySelectorAll('.faq-h1');
 const faqIcons = document.querySelectorAll('.faq-icon');
 const faqItems = document.querySelectorAll('.faq-items');
-const faqIcon = document.querySelectorAll('.faq-icon');
-for (let index = 0; index < faqIcons.length; index++) {
-	faqIcons[index].addEventListener('click', () => {
-		faqItems[index].classList.toggle('items-active');
-		faqP[index].classList.toggle('p-active');
-		faqIcon[index].classList.toggle('icon-active');
-		faqH1[index].classList.toggle('h1-active');
+
+for (let i = 0; i < faqIcons.length; i++) {
+	faqItems[i].addEventListener('click', () => {
+		if (faqItems[i].classList.contains('items-active')) {
+			faqItems[i].classList.remove('items-active');
+			faqH1[i].classList.remove('h1-active');
+			faqIcons[i].classList.remove('icon-active');
+			faqP[i].classList.remove('p-active');
+		} else {
+			removeFaqH1();
+			removeFaqIcons();
+			removeFaqItems();
+			removeFaqP();
+			faqItems[i].classList.add('items-active');
+			faqH1[i].classList.add('h1-active');
+			faqIcons[i].classList.add('icon-active');
+			faqP[i].classList.add('p-active');
+		}
+	});
+}
+
+function removeFaqP() {
+	faqP.forEach((ami) => {
+		ami.classList.remove('p-active');
+	});
+}
+function removeFaqH1() {
+	faqH1.forEach((b) => {
+		b.classList.remove('h1-active');
+	});
+}
+function removeFaqIcons() {
+	faqIcons.forEach((b) => {
+		b.classList.remove('icon-active');
+	});
+}
+
+function removeFaqItems() {
+	faqItems.forEach((c) => {
+		c.classList.remove('items-active');
 	});
 }
