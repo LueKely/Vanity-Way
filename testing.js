56624a179de195c7bc28aa1af1e9269ee9d168a2
LueKely <99306129+LueKely@users.noreply.github.com>
2022-05-31 17:30:58 +0800

    replaced the faq to a new and better one to style

--- testing.js
+++ testing.js
@@ -1,4 +1,5 @@
 const faqP = document.querySelectorAll('.faq-p');
+const faqH1 = document.querySelectorAll('.faq-h1');
 const faqIcons = document.querySelectorAll('.faq-icon');
 const faqItems = document.querySelectorAll('.faq-items');
 const faqIcon = document.querySelectorAll('.faq-icon');
@@ -7,5 +8,6 @@ for (let index = 0; index < faqIcons.length; index++) {
 		faqItems[index].classList.toggle('items-active');
 		faqP[index].classList.toggle('p-active');
 		faqIcon[index].classList.toggle('icon-active');
+		faqH1[index].classList.toggle('h1-active');
 	});
 }
